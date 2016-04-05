3c31b551ebb6bc7ae878abb35104938af645056e
SilasJeppe <Silas_Jeppe@hotmail.com>
2016-04-05 10:11:22 +0200

    models classes added

---
+++ webapi/Models/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace webapi.Models
+{
+    [DataContractAttribute]
+    public class Route
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public List<Point> PointList { get; set; }
+
+        public Route()
+        {
+
+        }
+    }
+}
---
+++ webapi/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace webapi.Models
+{
+    [DataContractAttribute]
+    public class User
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Firstname { get; set; }
+        [DataMember]
+        public string Surname { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public int ZipCode { get; set; }
+        [DataMember]
+        public int PhoneNumber { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string password { get; set; }
+        [DataMember]
+        public List<Activity> ActivityList { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
