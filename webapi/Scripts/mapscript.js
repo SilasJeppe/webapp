28822270762402c21ebaa809771222771a5ef6b3
Søren Kaae <soren.kaae@gmail.com>
2016-04-27 13:03:28 +0200

    can add more markers

--- webapi/Scripts/mapscript.js
+++ webapi/Scripts/mapscript.js
@@ -1,12 +1,17 @@
-﻿function initMap() {
+﻿var map;
+
+function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
+    var mapOptions = {
         center: { lat: 57.048820, lng: 9.921747 },
         zoom: 13,
         streetViewControl: false,
         mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    });
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        editable: true
+    };
+
+    map = new google.maps.Map(mapDiv, mapOptions);
     var latlng = { lat: 57.048820, lng: 9.921747 };
     var marker = new google.maps.Marker({
         position: latlng,
